0f42c9c557d853038bfcbff3b8182dbb43def0c6
sheng.chen <chensheng@shutuo.tv>
2026-06-02 09:24:06 +0800

    init

---
+++ src-tauri/gen/android/app/src/main/java/com/chens/s/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.chens.s
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
